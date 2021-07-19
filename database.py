942240532e72ff611fecef46e6bb73f6c22f1264
wyvern <amirhosein_wyvern@yahooo.com>
2021-07-19 13:52:51 +0430

    [database.py] create class DataBase with other method ,ready for use

--- database.py
+++ database.py
@@ -15,3 +15,64 @@ def _checkExcept(func):
             return False
     
     return wrapper
+
+
+class DataBase:
+
+    def __init__(self):
+
+        self.__REDIS = redis.StrictRedis(host='127.0.0.1', port=6379 ,decode_responses= True)
+        logging.warning( '*>Connected to DataBase ' )
+
+    @_checkExcept
+    def _get(self ,arg1):
+        try:
+            return json.loads( self.__REDIS.get(arg1) )
+        
+        except json.decoder.JSONDecodeError as e:
+            return self.__REDIS.get(arg1) 
+
+    @_checkExcept
+    def _hget(self ,arg1 ,arg2):
+        try:
+            return json.loads( self.__REDIS.hget(arg1 ,arg2) )
+            
+        except json.decoder.JSONDecodeError as e:
+            return self.__REDIS.hget(arg1 ,arg2)
+
+    @_checkExcept
+    def _set(self ,arg1 ,arg2):
+        
+        self.__REDIS.set(arg1 ,json.dumps(arg2))
+        return True
+
+    @_checkExcept
+    def _hset(self ,arg1 ,arg2 ,arg3):
+        
+        self.__REDIS.hset(arg1 ,arg2 ,json.dumps(arg3))
+        return True 
+
+    @_checkExcept
+    def _exists(self ,arg1):
+    
+        return self.__REDIS.exists(arg1 )
+
+    @_checkExcept
+    def _hexists(self ,arg1 ,arg2):
+        
+        return self.__REDIS.hexists(arg1 ,arg2)
+            
+    @_checkExcept
+    def _hgetall(self ,arg1):
+       
+        return self.__REDIS.hgetall(arg1)
+    
+    @_checkExcept
+    def _lrange(self ,arg1 ,start ,end):
+        
+        return self.__REDIS.lrange(arg1 ,start ,end)
+
+    @_checkExcept
+    def _Hkeys(self ,arg1):
+    
+        return self.__REDIS.hkeys(arg1)
