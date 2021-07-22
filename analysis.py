59f55f489b3cbf49d699edfab713a23ad79f71f9
wyvern <amirhosein_wyvern@yahooo.com>
2021-07-22 12:55:53 +0430

    Behavior change in analysis

--- analysis.py
+++ analysis.py
@@ -4,26 +4,90 @@ import pandas as pd
 class Indicator:
 
     @staticmethod
-    def rsi(pricePakage ,buy= 30 ,sell= 70 ,length= 14 ,source= 'close'):
+    def rsi(pricePackage ,buy= 30 ,sell= 70 ,length= 14 ,source= 'close' ,start_index = 0):
         
-        pd_price = pd.DataFrame(pricePakage)
+        pd_price = pd.DataFrame(pricePackage)
         rsi_data = pd_price.ta.rsi(close=source ,length=length)
 
-        xpoint = rsi_data.iloc[-1]
+        if start_index < length :
+            start_index = length
+            
+        res = []
 
-        if max(rsi_data.iloc[-4:]) >= sell:
-            return 'sell'
-        
-        elif min(rsi_data.iloc[-4:]) <= buy:
-            return 'buy'
+        for i in range(start_index , len(rsi_data)):
 
-        return None
+            maxPrice = max([ pricePackage[i]['open'] ,pricePackage[i]['close'] ,pricePackage[i]['high'] ,pricePackage[i]['low']])
+            minPrice = min([ pricePackage[i]['open'] ,pricePackage[i]['close'] ,pricePackage[i]['high'] ,pricePackage[i]['low']])
+
+            if min(rsi_data.iloc[i-4:i+1]) <= buy :
+                res.append({'type': 'buy' ,
+                            'rsi': rsi_data.iloc[i],
+                            'open': pricePackage[i]['open'] ,
+                            'close': pricePackage[i]['close'] ,
+                            'high': pricePackage[i]['high'] ,
+                            'low': pricePackage[i]['low'] })
+
+            elif max(rsi_data.iloc[i-4:i+1]) >= sell :
+                res.append({'type': 'sell' ,
+                            'rsi': rsi_data.iloc[i],
+                            'open': pricePackage[i]['open'] ,
+                            'close': pricePackage[i]['close'] ,
+                            'high': pricePackage[i]['high'] ,
+                            'low': pricePackage[i]['low'] })
+
+            else:
+                res.append({'type': 'None' ,
+                            'rsi': rsi_data.iloc[i],
+                            'open': pricePackage[i]['open'] ,
+                            'close': pricePackage[i]['close'] ,
+                            'high': pricePackage[i]['high'] ,
+                            'low': pricePackage[i]['low'] })
+
+        return res
+
 
     @staticmethod
-    def ma_vs_price(pricePakage):
+    def ma_vs_price(pricePackage ,buy= 30 ,sell= 70 ,length= 14 ,source= 'close' ,start_index = 0):
         
-        pd_price = pd.DataFrame(pricePakage)
-        ma_data = pd_price.ta.ma(close=source ,length=length)
+        pd_price = pd.DataFrame(pricePackage)
+        sma_data = pd_price.ta.sma(close=source ,length=length)
+
+        if start_index < length :
+            start_index = length
+
+        res = []
+
+        for i in range(start_index , len(sma_data)):
+
+            maxPrice = max([ pricePackage[i]['open'] ,pricePackage[i]['close'] ,pricePackage[i]['high'] ,pricePackage[i]['low']])
+            minPrice = min([ pricePackage[i]['open'] ,pricePackage[i]['close'] ,pricePackage[i]['high'] ,pricePackage[i]['low']])
+
+            if minPrice > sma_data.iloc[i] :
+                res.append({'type': 'buy' ,
+                            'ma': sma_data.iloc[i],
+                            'open': pricePackage[i]['open'] ,
+                            'close': pricePackage[i]['close'] ,
+                            'high': pricePackage[i]['high'] ,
+                            'low': pricePackage[i]['low'] })
+
+            elif maxPrice < sma_data.iloc[i] :
+                res.append({'type': 'sell' ,
+                            'ma': sma_data.iloc[i],
+                            'open': pricePackage[i]['open'] ,
+                            'close': pricePackage[i]['close'] ,
+                            'high': pricePackage[i]['high'] ,
+                            'low': pricePackage[i]['low'] })
+
+            else:
+                res.append({'type': 'None' ,
+                            'ma': sma_data.iloc[i],
+                            'open': pricePackage[i]['open'] ,
+                            'close': pricePackage[i]['close'] ,
+                            'high': pricePackage[i]['high'] ,
+                            'low': pricePackage[i]['low'] })
+
+        return res
+
 
     
 
